08295d6d740c92dee51d66600c61e5fa144aa1b4
SalcedoJose <josemasal@gmail.com>
2018-02-08 16:16:09 -0800

    Modificado inicio para subir archivo.

--- web/script/page/index.js
+++ web/script/page/index.js
@@ -2,6 +2,15 @@
 (function() {
 	"use strict";
 
-	// MAIN.
-	$(document).ready(function() {});
+	// CONSTANTES.
+	window.ARCHIVO_SUBIDA_FTP_ID = "#ArchivoSubidaFtp";
+	window.SUBIR_ARCHIVO_ID = "#SubirArchivo";
+
+	// PRINCIPAL.
+	$(document).ready(function() {
+		$(window.SUBIR_ARCHIVO_ID).click(function() {
+			// Obtener propiedades de archivo.
+			const archivo = $(ARCHIVO_SUBIDA_FTP_ID).prop("files")[0];
+		});
+	});
 }());
