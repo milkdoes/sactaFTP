058feaa117288cfbdfa82f434134002b72730332
SalcedoJose <josemasal@gmail.com>
2018-02-12 18:01:20 -0800

    Creacion de vinculo simbolico para el folder de web

--- ftp/config/modificarServidorWeb.sh
+++ ftp/config/modificarServidorWeb.sh
@@ -8,13 +8,18 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Constantes.
-USUARIO=sacta
+USUARIO="sacta"
 CONTRASENA="Sacta1"
-DIR_FTP=/home/$USUARIO/ftp
-DIR_TEMP=$DIR_FTP/temp 
+DIR_FTP="/home/$USUARIO/ftp"
+DIR_TEMP="$DIR_FTP/temp"
+DIR_WEB="$(cd $(pwd)/../../web; pwd)"
+DIR_SITIO_WEB="/var/www/html/sactaftp"
 
 # Crear directorio para archivos temporales.
 mkdir $DIR_TEMP
 
 # Permitir que cualquier usuario escriba archivos en el folder temporal.
 chmod 777 -R $DIR_TEMP/
+
+# Crear vinculo simbolico a folder de web.
+ln -s $DIR_WEB $DIR_SITIO_WEB
