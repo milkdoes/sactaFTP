323f2fc3a62ba6b3fc8a21fcc2bd1f3f0780b952
ramirez-luis <luisfer24680@gmail.com>
2018-02-26 18:44:29 -0800

    Invocacion e instanciacion de clase ftp_class en home.php

--- web/home.php
+++ web/home.php
@@ -1,13 +1,20 @@
 <?php
 session_start();
+$user = $_SESSION['ftp_user'];
+$pass = $_SESSION['ftp_pass'];
+
 // set up basic connection
 $conn_id = ftp_connect('localhost');
 
+// *** Include the class.
+include('script/data/ftp_class.php');
+$ftpObj = new FTPClient();
+
 // login with username and password
-$login_result = ftp_login($conn_id, $_SESSION['ftp_user'], $_SESSION['ftp_pass']);
+$login_result = ftp_login($conn_id, $user, $pass);
 
 // get the file list for /
-$buff = ftp_nlist($conn_id, '/');
+$buff = $ftpObj -> getDirListing('.', '');
 //$buff2 = ftp_n
 
 // close the connection
