ba7d7b5bdfa422cc95556805d211651ff3c35cd1
SalcedoJose <josemasal@gmail.com>
2018-04-17 14:20:05 -0700

    Compartimiento de archivos exitoso, mientras se pongan lineas de control en el archivo de sudoers.

--- web/script/data/compartirArchivos.php
+++ web/script/data/compartirArchivos.php
@@ -1,20 +1,59 @@
 <?php
+// INSTRUCCIONES: Tener estas lineas al final del archivo "sudoers" para
+// ejecutar correctamente.
+/*
+Defaults:vsftpd !requiretty
+Defaults:www-data !requiretty
+www-data ALL=(vsftpd) NOPASSWD: ALL
+*/
+// Linea de prueba:
+// http://localhost/sactaftp/script/data/compartirArchivos.php?usuarioHuesped=user1&usuarioPatrocinador=pepe&archivos[]=macOS.jpg
+
 // Start session if it has not already started.
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+// CONSTANTES.
+define("USUARIO_FTP", "vsftpd");
+define("RUTA_TERMINAL", "/bin/bash");
+define("SCRIPT_COMPARTIR", "compartirParaUsuario.sh");
+
 //Recibir los valores del POST
-$usuarioHuesped = $_POST['usuarioHuesped'];
-$usuarioPatrocinador = $_POST['usuarioPatrocinador'];
-$archivos = $_POST['archivos']; //Array de archivos
+$usuarioHuesped = "";
+$usuarioHuesped = $_REQUEST['usuarioHuesped'];
+$usuarioPatrocinador = "";
+$usuarioPatrocinador = $_REQUEST['usuarioPatrocinador'];
+$archivos = array();
+$archivos = $_REQUEST['archivos']; //Array de archivos
 
-//Mensajes debug
-echo $usuarioHuesped;
-echo ' ' . $usuarioPatrocinador . ' ';
-var_dump($archivos);
+// Definir si la lista de parametros enviados son validos.
+$parametrosValidos = empty($usuarioHuesped) == false &&
+	empty($usuarioPatrocinador) == false && empty($archivos) == false;
 
-//Compartir archivos
-//---Ejecutar el bash aqui---
+// Continuar compartiendo archivos si los parametros a usar son validos.
+$json = array(
+	"mensaje" => "Error al enviar parametros."
+	, "codigoSalida" => 1
+	, "parametrosEnviados" => json_encode($_REQUEST)
+	, "mensajesTerminal" => array()
+);
+if ($parametrosValidos) {
+	// Definir lista de archivos como una sola linea de texto.
+	$listaArchivos = implode(" ", $archivos);
 
+	// Definir lista de parametros.
+	$parametros = "$usuarioHuesped $usuarioPatrocinador $listaArchivos";
+
+	// Ejecutar compartimiento de archivos.
+	exec("sudo -u " . USUARIO_FTP . " " . RUTA_TERMINAL . " " .
+		SCRIPT_COMPARTIR .  " $parametros 2>&1",
+		$lineasSalida, $codigoSalida);
+	$json["mensaje"] = "Script ejecutado.";
+	$json["codigoSalida"] = $codigoSalida;
+	$json["mensajesTerminal"] = implode("<br />", $lineasSalida);
+}
+
+// Desplegar json.
+echo json_encode($json);
 ?>
