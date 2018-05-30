c7340276b744118758efdfa8386db34a00a8f700
SalcedoJose <josemasal@gmail.com>
2018-05-29 19:47:08 -0700

    Archivo de creacion de usuario virtual actualizado.

--- ftp/config/creacionUsuarioVirtual.sh
+++ ftp/config/creacionUsuarioVirtual.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Archivo para crear un usuario virtual de ftp.
 
+# Parametros: usuario contraseña
+
 # Verificar si usuario es super usuario. Si no salir.
 if [ "$EUID" -ne 0 ]
 	then echo "Por favor correr como superusuario (sudo)."
@@ -17,10 +19,24 @@ NOMBRE_DIRECTORIO_COMPARTIDOS="compartidos"
 # PRINCIPAL/MAIN.
 # Definir credenciales y rutas de directorio y archivo de configuracion
 # de usuario virtual.
-echo "Ingrese nombre de usuario virtual: "
-read USUARIO_VIRTUAL
-echo "Ingrese contraseña de usuario virtual ($USUARIO_VIRTUAL): "
-read CONTRASENA_VIRTUAL
+
+# Usuario.
+if [ -z "$1" ]; then
+	echo "Ingrese nombre de usuario virtual: "
+	read USUARIO_VIRTUAL
+else
+	USUARIO_VIRTUAL="$1"
+fi
+
+# Contraseña.
+if [ -z "$2" ]; then
+	echo "Ingrese contraseña de usuario virtual ($USUARIO_VIRTUAL): "
+	read CONTRASENA_VIRTUAL
+else
+	CONTRASENA_VIRTUAL="$2"
+fi
+
+# Definir rutas de archivos y directorios a utilizar.
 DIRECTORIO_VIRTUAL="$DIRECTORIO_FTP_USUARIO/$USUARIO_VIRTUAL"
 ARCHIVO_VIRTUAL="$DIRECTORIO_CONFIGURACION_USUARIOS_VIRTUALES/$USUARIO_VIRTUAL"
 
